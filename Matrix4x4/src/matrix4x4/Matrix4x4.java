3b39076496860bba651fcae394ed183ee8e1782d
BenBrassard94 <62662981+BenBrassard94@users.noreply.github.com>
2020-04-01 22:46:39 -0600

    Testing if this shows up on git

--- Matrix4x4/src/matrix4x4/Matrix4x4.java
+++ Matrix4x4/src/matrix4x4/Matrix4x4.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package matrix4x4;
 
 /**
@@ -11,11 +6,35 @@ package matrix4x4;
  */
 public class Matrix4x4 {
 
-    /**
-     * @param args the command line arguments
-     */
+    private double[][] elements = new double[4][4];
+
+    public Matrix4x4() {
+        this.identity();
+    } // Operator()
+
+    public double get(int row, int column) {
+        return this.elements[row][column];
+    } // get( int, int )
+
+    public void set(int row, int column, double value) {
+        this.elements[row][column] = value;
+    } // set( int, int, double )
+
+    public final void identity() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i == j) {
+                    this.set(i, j, 1.0);
+                } // if
+                else {
+                    this.set(i, j, 0.0);
+                } // else
+            } // for
+        } // for
+    } // identity()
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
-    
+
 }
